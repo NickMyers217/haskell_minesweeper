4b7c5133813b14f1c480cf5bdb45d39cba4be8c7
nickmyers217 <nickbmyers217@gmail.com>
2015-10-25 20:40:21 -0700

    Implemented most of the cell module functions, now working on the board module.

--- src/Board.hs
+++ src/Board.hs
@@ -6,12 +6,39 @@ module Board where
 import Cell
 
 -- | A Size is the (cols, rows) for a board
-type Size = (Int, Int)
+type Size  = (Int, Int)
 
--- | A Board is a grid of Maybe Cells
-type Board = [[Maybe Cell]]
+-- | A Point is an (x, y) coordinate
+type Pos   = (Int, Int)
+
+-- | A Board is a grid of Cells
+type Board = [[Cell]]
+
 
 -- | Creates a new Board of a given Size
 newBoard :: Size -> Board
-newBoard (cols,rows) = [ [ Nothing | c <- [1..cols] ]
-                                   | r <- [1..rows] ]
+newBoard (cols,rows) = [ [ Empty Hidden | c <- [1..cols] ]
+                                        | r <- [1..rows] ]
+
+-- | Get a Cell from a Board at a given Pos
+getCell :: Board -> Pos -> Cell
+getCell b (x,y) = b !! y !! x
+
+
+-- | Update a list with a new value at a given index
+(!!=) :: [a] -> (Int, a) -> [a]
+(!!=) xs (i,v) 
+    | i > -1 && i < length xs = take i xs ++ [v] ++ drop (i + 1) xs
+    | otherwise               = error "Can't modify the list at the given index."
+
+
+-- | Change a Board's Cell by using a given function at that Pos
+changeCell :: Board -> (Cell -> Cell) -> Pos -> Board
+changeCell b f (x, y) = b !!= (y, row !!= (x, f cell))
+    where row  = b !! y
+          cell = row !! x
+
+
+-- | Flags a cell on the board
+flagBoardCell :: Board -> Pos -> Board
+flagBoardCell b = changeCell b flagCell
--- src/Cell.hs
+++ src/Cell.hs
@@ -3,14 +3,54 @@
 -}
 module Cell where
 
--- | A Point is an (x, y) coordinate
-type Pos  = (Int, Int)
+-- | A cell can be Hidden or Shown
+data Visibility = Hidden
+                | Shown
+                deriving (Eq, Show)
 
 -- | A Cell can be one of the following:
 -- | A Number 1 through 8
 -- | A Bomb
--- | A Flag ontop of either a Number or Bomb
-data Cell = Num Int
-          | Bomb
-          | Flag Cell
+-- | Empty
+-- | A Flag ontop of any of the previous
+data Cell = Num Int Visibility
+          | Bomb Visibility
+          | Empty Visibility
+          | Flag  Cell
           deriving (Eq, Show)
+
+-- | Determine if a Cell is Shown
+isShown :: Cell -> Bool
+isShown c =
+    case c of
+      Num _ Shown -> True
+      Bomb  Shown -> True
+      Empty Shown -> True
+      Flag  cell  -> isShown cell
+      _           -> False
+
+-- | Handles the flagging of cells:
+-- | Flags a Hidden Num, Bomb, or Empty
+-- | Unflags a Flag
+-- | Does nothing to Shown Num, Bomb, or Empty
+flagCell :: Cell -> Cell
+flagCell c =
+    case c of
+      n@(Num _ Hidden) -> Flag n
+      b@(Bomb Hidden)  -> Flag b
+      e@(Empty Hidden) -> Flag e
+      Flag cell        -> cell
+      _                -> c
+
+-- | Handles the revealing of cells:
+-- | Reveals a Hidden Num, Bomb, or Empty
+-- | Does nothing to a Flag
+-- | Does nothing to a Shown Num, Bomb, or Empty
+revealCell :: Cell -> Cell
+revealCell c =
+    case c of
+      Num i Hidden -> Num i Shown
+      Bomb Hidden  -> Bomb Shown
+      Empty Hidden -> Empty Shown
+      _            -> c
+
